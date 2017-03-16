3b225e386f7684ab487b516963eef6a87b3eca5a
siarhei-luskanau <siarhei.luskanau@gmail.com>
2017-03-16 14:29:03 +0300

    Linked firebase reference and switch view

--- index.android.js
+++ index.android.js
@@ -1,16 +1,43 @@
+'use strict';
+
 import React, { Component } from 'react';
-import {
+import ReactNative, {
   AppRegistry,
   StyleSheet,
   Text,
+  Switch,
   View
 } from 'react-native';
-import * as firebase from "firebase";
+import Firebase from "firebase";
+
+Firebase.initializeApp({
+ apiKey: "AIzaSyA5rK5JDZFjY5SpIL5FwlXhx8Sty-k9FBs",
+    authDomain: "fir-database-f6b5a.firebaseapp.com",
+    databaseURL: "https://fir-database-f6b5a.firebaseio.com",
+    storageBucket: "fir-database-f6b5a.appspot.com",
+    messagingSenderId: "443316178527"
+});
 
 export default class ReactNativeIotLamp extends Component {
+
+  state = {
+    lampSwitch: false,
+  };
+
+  lampRef = Firebase.database().ref('lamp');
+
+  listenForLampRef() {
+    this.lampRef.on('value', (snapshot) => {
+      this.setState({lampSwitch: snapshot.val()});
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
+        <Switch
+          onValueChange={(value) => this.lampRef.set(value)}
+          value={this.state.lampSwitch} />
         <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
@@ -24,15 +51,11 @@ export default class ReactNativeIotLamp extends Component {
       </View>
     );
   }
-}
 
-firebase.initializeApp({
- apiKey: "AIzaSyA5rK5JDZFjY5SpIL5FwlXhx8Sty-k9FBs",
-    authDomain: "fir-database-f6b5a.firebaseapp.com",
-    databaseURL: "https://fir-database-f6b5a.firebaseio.com",
-    storageBucket: "fir-database-f6b5a.appspot.com",
-    messagingSenderId: "443316178527"
-});
+  componentDidMount() {
+    this.listenForLampRef();
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
